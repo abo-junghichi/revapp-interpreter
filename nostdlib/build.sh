e7d2117bcb019363a7f4360997db81ca0794beea
Junghichi Abo <abo_junghichi@yahoo.co.jp>
2023-06-22 23:41:49 +0900

    simplify nostdlib/build.sh

--- nostdlib/build.sh
+++ nostdlib/build.sh
@@ -1,7 +1,4 @@
 #!/bin/dash
-for f in revappi.h revappi.c main.c romsrc.c
-do
-	cp ../$f .
-done
+cp ../revappi.h ../revappi.c ../main.c ../romsrc.c .
 patch < nostdlib.patch
 gcc -Os -Wall -pedantic -Wextra -Wstrict-aliasing=1 -static -nostdlib system-i386.c revappi.c main.c -o revappi.out
